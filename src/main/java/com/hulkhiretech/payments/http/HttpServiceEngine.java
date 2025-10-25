f63ed3c4741035b9e5c91ddb0868a11c80da9e23
Ashwani kumar yadav <kumar3011ashwani@gmail.com>
2025-10-25 17:00:05 +0530

    Code Updated with new functionality

--- src/main/java/com/hulkhiretech/payments/http/HttpServiceEngine.java
+++ src/main/java/com/hulkhiretech/payments/http/HttpServiceEngine.java
@@ -2,16 +2,9 @@ package com.hulkhiretech.payments.http;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
-
-import java.util.Base64;
-import java.util.function.Consumer;
 
 @Component
 @Slf4j
@@ -20,44 +13,30 @@ public class HttpServiceEngine {
 
     private final RestClient restClient;
 
-    public String makeHttpCall() {
+    public ResponseEntity<String> makeHttpCall(HttpRequest httpRequest) {
         log.info("Making HTTP call in HttpServiceEngine");
 
-        HttpHeaders headers = new HttpHeaders();
 
-        String clientId = "AWo5eGT8dJ8z4wv295DWA8WNSidMcTLlPxQDEr1ZusqkdbOIIELPPTcg-hGGWoadE5aljHJCgl5ZB_sN";
-        String clientSecret = "EBkqh3eJB82fK3r0H4heuxG6wUrcHrzBlmOBAT1vREmfMUBtVw5_JjYtdJTjqME3LjP4SDtDnCaFqlen";
+        try {
+            ResponseEntity<String> httpResponse = restClient
+                    .method(httpRequest.getHttpMethod())
+                    .uri(httpRequest.getUrl())
+                    .headers(
+                            restClientHeaders ->
+                                    restClientHeaders.addAll(
+                                            httpRequest.getHttpHeaders()))
+                    .body(httpRequest.getBody())
+                    .retrieve()
+                    .toEntity(String.class);
 
-        headers.setBasicAuth(clientId, clientSecret);
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            log.info("HTTP call completed in HttpResponse : {}", httpResponse);
 
-class ConsumerHeaderObj implements Consumer<HttpHeaders> {
+            return httpResponse;
+        } catch (Exception e) {
+            log.error("Exception while preparing from data : {}", e.getMessage());
 
-    HttpHeaders applicationHeaders;
+            throw new RuntimeException("HTTP call failed in httpServiceEngine " + ":" + e.getMessage());
+        }
 
-    ConsumerHeaderObj(HttpHeaders applicationHeaders) {
-        this.applicationHeaders = applicationHeaders;
-    }
-
-    @Override
-    public void accept(HttpHeaders restClientHeaders) {
-        restClientHeaders.addAll(this.applicationHeaders);
-
-    }
-}
-
-        MultiValueMap<String , String> formData = new LinkedMultiValueMap<>();
-       // formData.add("grant_type", "client_credentials");
- 
-        String httpResponse = restClient.method(HttpMethod.POST)
-                .uri("https://api-m.sandbox.paypal.com/v1/oauth2/token")
-                .headers(new  ConsumerHeaderObj(headers))
-                .body(formData)
-                .retrieve()
-                .body(String.class);
-
-                log.info("HTTP call completed in HttpResponse : {}" ,httpResponse );
-
-        return httpResponse;
     }
 }
--- src/main/java/com/hulkhiretech/payments/service/TokenService.java
+++ src/main/java/com/hulkhiretech/payments/service/TokenService.java
@@ -1,9 +1,18 @@
 package com.hulkhiretech.payments.service;
 
+import com.hulkhiretech.payments.constant.Constant;
+import com.hulkhiretech.payments.http.HttpRequest;
 import com.hulkhiretech.payments.http.HttpServiceEngine;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @Service
 @Slf4j
@@ -13,6 +22,15 @@ public class TokenService {
     private final HttpServiceEngine httpServiceEngine;
     private static String accessToken;
 
+    @Value("${paypal.client.id}")
+    private String clientId;
+
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+
+    @Value("${paypal.oauth.url}")
+    private String oAuth;
+
     public String getAccessToken() {
         log.info("Retrieving access token from TokenService");
         if (accessToken != null) {
@@ -21,9 +39,27 @@ public class TokenService {
         }
         log.info("No cached token found, calling OAuth service ");
 
-        String response = httpServiceEngine.makeHttpCall();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setBasicAuth(clientId, clientSecret);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add(Constant.GRANT_TYPE, Constant.CLIENT_CREDENTIALS);
+
+        HttpRequest httpRequest = new HttpRequest();
+        httpRequest.setUrl(oAuth);
+        httpRequest.setHttpMethod(HttpMethod.POST);
+        httpRequest.setHttpHeaders(headers);
+        httpRequest.setBody(formData);
+
+        log.info("Prepared HttpRequest for OAuth call : {}", httpRequest);
+
+        ResponseEntity<String> response = httpServiceEngine.makeHttpCall(httpRequest);
         log.info("HTTP response from httpServiceEngine: {}", response);
 
-        return "access_token - " + response;
+        String tokenBody = response.getBody();
+
+        return tokenBody;
     }
 }
