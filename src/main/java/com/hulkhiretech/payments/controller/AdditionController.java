10af10aa3417447d7b94d079d87782d13805f294
ASHWANI KUMAR YADAV <kumar3011ashwani@gmail.com>
2025-11-14 09:51:21 +0530

    Paypal service getting started

--- src/main/java/com/hulkhiretech/payments/controller/AdditionController.java
+++ src/main/java/com/hulkhiretech/payments/controller/AdditionController.java
@@ -1,4 +1,4 @@
-package com.hulkhiretech.payments.controller;
+package com.ashwaniPaypalService.payments.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/test/java/com/hulkhiretech/payments/PaypalProviderServiceApplicationTests.java
+++ src/test/java/com/hulkhiretech/payments/PaypalProviderServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hulkhiretech.payments;
+package com.ashwaniPaypalService.payments;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
